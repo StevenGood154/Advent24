68e988c9816a40fb38c2d3d12c21a0d96edbd8e0
Steven Good <steve.good.154@gmail.com>
2024-12-05 22:46:03 -0500

    Day 3: Part 1

---
+++ Advent24/Day3/MullItOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent24.Day3;
+
+public static partial class MullItOver
+{
+    private static readonly string FilePath = Path.Combine(Environment.CurrentDirectory, "Input/day3.txt");
+
+    public static int Part1()
+    {
+        var fileContents = Parse();
+        
+        var regex = MultiplyRegex();
+        var matches = regex.Matches(fileContents);
+        
+        var products = matches.Select(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+        return products.Sum();
+    }
+    
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+    private static partial Regex MultiplyRegex();
+
+    private static string Parse()
+    {
+        return File.ReadAllText(FilePath);
+    }
+}
+
